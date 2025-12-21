a6314b24d672c76de5904a8df4dab85392804043
liushu-ye <1494857286@qq.com>
2025-12-21 21:29:33 +0800

    整理Tailwind CSS文件

--- qianduan/vite.config.ts
+++ qianduan/vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  css: {
+    postcss: {
+      config: {
+        path: './config/tailwind/postcss.config.js'
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
